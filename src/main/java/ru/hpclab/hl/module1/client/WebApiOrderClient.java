4d5998fbfd58a0d00e249556a4e5497f9afece98
geizerde <geizerde@gmail.com>
2025-04-24 09:16:21 +0300

    lab10.9: add Observability statistics and update version on docker-compose

--- src/main/java/ru/hpclab/hl/module1/client/WebApiOrderClient.java
+++ src/main/java/ru/hpclab/hl/module1/client/WebApiOrderClient.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.hpclab.hl.module1.dto.OrderDTO;
+import ru.hpclab.hl.module1.service.statistics.ObservabilityService;
 
 import java.util.List;
 
@@ -11,13 +12,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebApiOrderClient {
     private final WebClient coreServiceWebClient;
+    private final ObservabilityService observabilityService;
 
     public List<OrderDTO> findAll() {
-        return coreServiceWebClient.get()
+        observabilityService.start(getClass().getSimpleName() + ":findAll");
+
+        List<OrderDTO> orders = coreServiceWebClient.get()
                 .uri("/orders")
                 .retrieve()
                 .bodyToFlux(OrderDTO.class)
                 .collectList()
                 .block();
+
+        observabilityService.stop(getClass().getSimpleName() + ":findAll");
+
+        return orders;
     }
 }
--- src/main/java/ru/hpclab/hl/module1/controller/OrderController.java
+++ src/main/java/ru/hpclab/hl/module1/controller/OrderController.java
@@ -1,24 +1,30 @@
 package ru.hpclab.hl.module1.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hpclab.hl.module1.model.order.OrderCustomerPrice;
 import ru.hpclab.hl.module1.service.OrderService;
+import ru.hpclab.hl.module1.service.statistics.ObservabilityService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
     private final OrderService service;
-
-    public OrderController(OrderService service) {
-        this.service = service;
-    }
+    private final ObservabilityService observabilityService;
 
     @GetMapping("/total-prices")
     public List<OrderCustomerPrice> calculateTotalOrdersPrices() {
-        return service.calculateTotalOrdersPrices();
+        observabilityService.start(getClass().getSimpleName() + ":calculateTotalOrdersPrices");
+
+        List<OrderCustomerPrice> result = service.calculateTotalOrdersPrices();
+
+        observabilityService.stop(getClass().getSimpleName() + ":calculateTotalOrdersPrices");
+
+        return result;
     }
 }
