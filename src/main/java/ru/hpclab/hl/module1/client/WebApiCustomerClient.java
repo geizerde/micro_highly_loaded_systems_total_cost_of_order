25192cb333379774725aaf603be2fe41d7f39816
geizerde <geizerde@gmail.com>
2025-04-29 23:44:07 +0300

    lab17-var2: add circuitbreaker

--- src/main/java/ru/hpclab/hl/module1/client/WebApiCustomerClient.java
+++ src/main/java/ru/hpclab/hl/module1/client/WebApiCustomerClient.java
@@ -1,16 +1,11 @@
 package ru.hpclab.hl.module1.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import ru.hpclab.hl.module1.model.Customer;
-import ru.hpclab.hl.module1.model.Product;
 import ru.hpclab.hl.module1.service.statistics.ObservabilityService;
-
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,10 +13,8 @@ public class WebApiCustomerClient {
     private final WebClient coreServiceWebClient;
     private final ObservabilityService observabilityService;
 
-    @CircuitBreaker(name = "CORE_SERVICE", fallbackMethod = "fallback")
-    @Retry(name = "CORE_SERVICE")
+    @CircuitBreaker(name = "CORE_SERVICE")
     public Customer findCustomerById(Long id) {
-        System.out.println("findCustomerById");
         observabilityService.start(getClass().getSimpleName() + ":findCustomerById");
 
         Customer customer = coreServiceWebClient.get()
@@ -33,10 +26,5 @@ public class WebApiCustomerClient {
         observabilityService.stop(getClass().getSimpleName() + ":findCustomerById");
 
         return customer;
-    }
-
-    private Customer fallback(Long id, Exception e) {
-        System.out.println("Fallback for findProductById triggered: " + e.getMessage());
-        return null;
     }
 }
